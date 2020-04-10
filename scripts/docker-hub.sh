2f62da1d52ab5191cf3514754db7cf631404a69f
cuzz22000 <cwilson.jdev@gmail.com>
2020-04-10 07:40:04 -0400

    latest tag deployed last so always on top of hub list

--- scripts/docker-hub.sh
+++ scripts/docker-hub.sh
@@ -4,5 +4,5 @@ echo "$DOCKER_PWD" | docker login --username "$DOCKER_USR" --password-stdin
 docker build -t $IMAGE_NAME:latest .
 docker images
 docker tag $IMAGE_NAME:latest $IMAGE_NAME:$TRAVIS_TAG
-docker push $IMAGE_NAME:latest
 docker push $IMAGE_NAME:$TRAVIS_TAG
+docker push $IMAGE_NAME:latest
